643de7c88199069866c43a20d89b56235adba46c
Lui <luivloureiro@gmail.com>
2023-02-11 22:07:50 -0300

    Adicionando Pasta Assets do Projeto e Pasta Components no FrontEnd para a Criação do Navbar

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Container, Navbar, Nav, NavDropdown, Form, Button } from 'react-bootstrap'
+
+const Header = () => {
+    return (
+        <div>
+<     Navbar expand="lg">
+      <Container fluid>
+        <Navbar.Brand style={{ color: 'white' }} href="#">SuperaGames</Navbar.Brand>
+        <Navbar.Toggle style={{ color: 'white' }} aria-controls="navbarScroll" />
+        <Navbar.Collapse style={{ color: 'white' }} id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link style={{ color: 'white' }} href="#action1">Loja</Nav.Link>
+            <Nav.Link style={{ color: 'white' }} href="#action2">Cadastro</Nav.Link>
+            <Nav.Link style={{ color: 'white' }} href="#action2">Meus Pedidos</Nav.Link>
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Procurar Jogo"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+        </div>
+    );
+}
+
+export default Header;
